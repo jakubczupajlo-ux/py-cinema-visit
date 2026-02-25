de81d02369853532b0aca6fc0ea5cb14116f3e43
Jakub <jakubczupajlo@gmail.com>
2026-02-25 22:04:26 +0100

    fix cinema visit

--- app/main.py
+++ app/main.py
@@ -1,27 +1,25 @@
-from app.cinema.bar import CinemaBar
+from app.bar.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(
-        customers: list[dict],
-        hall_number: int,
-        cleaner: str,
-        movie: str
+    customers: list[dict],
+    hall_number: int,
+    cleaner: str,
+    movie: str
 ) -> None:
-    # Tworzenie instancji personelu i sali
     cleaner_instance = Cleaner(cleaner)
     hall_instance = CinemaHall(hall_number)
+    bar_instance = CinemaBar()
 
-    # Tworzenie instancji klientów i sprzedaż produktów w barze
     customer_instances = []
     for cust_dict in customers:
         new_customer = Customer(cust_dict["name"], cust_dict["food"])
         customer_instances.append(new_customer)
-        CinemaBar.sell_product(new_customer.food, new_customer)
+        bar_instance.sell_product(new_customer.food, new_customer)
 
-    # Rozpoczęcie seansu
     hall_instance.movie_session(
         movie_name=movie,
         customers=customer_instances,
